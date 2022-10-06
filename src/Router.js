296655275055027badfcbbb04017bd42ff3d52c6
yoonsue <mrokr0328@gmail.com>
2022-10-06 16:04:25 +0900

    컨펌 내용 수정 완료

--- src/Router.js
+++ src/Router.js
@@ -3,27 +3,27 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/reset.scss';
 import './styles/common.scss';
 
-// //최현님의 컴포넌트
-// import LoginHyun from './pages/choihyun/Login/Login';
-// import MainHyun from './pages/choihyun/Main/Main';
+//최현님의 컴포넌트
+import LoginHyun from './pages/choihyun/Login/Login';
+import MainHyun from './pages/choihyun/Main/Main';
 
-// //동선님의 컴포넌트
-// import LoginSeon from './pages/dongseon/Login/Login';
-// import MainSeon from './pages/dongseon/Main/Main';
+//동선님의 컴포넌트
+import LoginSeon from './pages/dongseon/Login/Login';
+import MainSeon from './pages/dongseon/Main/Main';
 
-// //민경님의 컴포넌트
-// import LoginMin from './pages/minkyung/Login/Login';
-// import MainMin from './pages/minkyung/Main/Main';
+//민경님의 컴포넌트
+import LoginMin from './pages/minkyung/Login/Login';
+import MainMin from './pages/minkyung/Main/Main';
 
-// //윤수의 컴포넌트
-// import LoginSue from './pages/yoonsue/Login/Login';
-// import MainSue from './pages/yoonsue/Main/Main';
+//윤수의 컴포넌트
+import LoginSue from './pages/yoonsue/Login/Login';
+import MainSue from './pages/yoonsue/Main/Main';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/" element={<LoginSue />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/choihyun/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginHyun() {
+  return (
+    <div>
+      <h1>안녕하세요</h1>
+    </div>
+  );
+}
+
+export default LoginHyun;
