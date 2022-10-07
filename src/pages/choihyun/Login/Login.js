561e46ef57ec302c64eb833b92afab7407169d5c
Hyun Choi <hyun@Hyunui-MacBookAir.local>
2022-10-07 10:11:19 +0900

    프로젝트 이동 및 사진 이동, 사진 경로 수정

--- src/pages/choihyun/Login/Login.js
+++ src/pages/choihyun/Login/Login.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./Login.scss"
 
-function LoginHyun() {
+const LoginHyun = () => {
+  let [idInput, setInputId] = useState("")
+  let [pwdInput, setInputPwd] = useState("")
+  let navigate = useNavigate()
+
+  const successLogin = () => {
+    idInput.includes('@') && pwdInput.length>5 ? navigate('/main') :
+    alert('id와 password를 올바르게 입력하세요.')
+    document.querySelectorAll('input')[0].value = ""
+    document.querySelectorAll('input')[1].value = ""
+  }
+  
+  const totalValudation = (event) => {
+    event.key == "Enter" ? successLogin() : console.log()
+  }
+
+
+
   return (
-    <div>
-      <h1>안녕하세요 최현 로그인 페이지입니다.</h1>
+    <div className="LogInContainer">
+      <div className="LoginBox">
+        <h1 className="logo">Westagram</h1>
+        <div className="inputBox">
+          <input
+            type="text"
+            id="id" 
+            name="id"
+            className="LogInInput"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={(event) => setInputId(event.target.value)}
+          />
+          <input
+            type="password"
+            id="password"
+            className="LogInInput"
+            placeholder="비밀번호"
+            onChange={(event) => setInputPwd(event.target.value)}
+            onKeyDown={totalValudation}
+          />
+        </div>
+        {idInput.length > 0 && pwdInput.length > 0 ?
+          <button id="LogInButton" onClick={successLogin}>
+            로그인
+          </button>
+          : <button id="LogInButton" disabled>
+            로그인
+          </button>
+        }
+        <a id="ForgotPassword" href="https://www.naver.com">
+          비밀번호를 잊으셨나요?
+        </a>
+      </div>
     </div>
-  );
+
+  )
 }
 
 export default LoginHyun;
--- src/pages/choihyun/Main/Main.js
+++ src/pages/choihyun/Main/Main.js
@@ -1,11 +1,189 @@
-import React from 'react';
+import React, {useState} from "react";
+import "./Main.scss"
 
-function MainHyun() {
+const MainHyun = () => {
+  let [commentInput, setCommentIput] = useState("")
+  let [commentInputArr, setCommentIputArr] = useState([])
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    let copyCommentInputArr = [...commentInput]
+    setCommentIputArr(copyCommentInputArr)
+    console.log(commentInputArr)
+
+  }
   return (
-    <div>
-      <h1>안녕하세요 최현 메인 페이지입니다.</h1>
+    <>
+  <div className="nav">
+    <div className="logoWrap">
+      <i id="logoIcon" className="fa-brands fa-instagram fa-2x" />
+      <h1 id="logoName">Westagram</h1>
     </div>
+    <div className="searchWrap">
+      <i className="fa-solid fa-magnifying-glass" />
+      <input className="search" placeholder="검색" />
+    </div>
+    <div className="naviconWrap">
+      <i className="fa-regular fa-compass fa-2x" />
+      <i className="fa-regular fa-heart fa-2x" />
+      <i id="visibleMenu" className="fa-regular fa-user fa-2x" />
+      <div className="profileMenu">
+        <div style={{ borderBottom: "1px solid #e0e0e0" }}>
+          <ul>
+            <li>
+              <a className="itemWrap">
+                <i className="fa-regular fa-user" />
+                <p>프로필</p>
+              </a>
+            </li>
+            <li>
+              <a className="itemWrap">
+                <i className="fa-regular fa-bookmark" />
+                <p>저장됨</p>
+              </a>
+            </li>
+            <li>
+              <a className="itemWrap">
+                <i className="fa-regular fa-wrench" />
+                <p>설정</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div style={{ padding: 10, borderBottom: "1px solid #e0e0e0" }}>
+          <a href="login.html" className="logout">
+            로그아웃
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="main">
+    <div className="feeds">
+      <div className="article">
+        <div className="feedWrap">
+          <div className="account">
+            <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/profile-insta.jpeg" className="profile" />
+            <h3>hyun._.gus</h3>
+          </div>
+          <div className="dot">
+            <h2>…</h2>
+          </div>
+        </div>
+        <div className="imgWrap">
+          <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/7stars-insta.jpeg" />
+        </div>
+        <div className="feedIcon">
+          <div className="leftIconWrap">
+            <i className="fa-solid fa-heart fa-2x" style={{ color: "red" }} />
+            <i className="fa-regular fa-message fa-2x" />
+            <i className="fa-solid fa-arrow-up-from-bracket fa-2x" />
+          </div>
+          <div className="rightIconWrap">
+            <i className="fa-regular fa-bookmark fa-2x" />
+          </div>
+        </div>
+        <div className="feedDesc">
+          <div>
+            <p className="ptagID">hyun._.gus</p>
+            <p>대천에서 찍은 북두칠성 . . .</p>
+            <p id="moreDesc">&nbsp; &nbsp;더 보기</p>
+          </div>
+          <div>
+            <p className="ptagID">wecode.bootcamp</p>
+            <p>어머~ 너무 이뻐용 ㅎㅎㅎ</p>
+            <span className="like">♡</span>
+            <span className="delete">X</span>
+          </div>
+        </div>
+        <div className="comment">
+          <div className="emoji">
+            <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/emoji.png" />
+          </div>
+          <form onSubmit={onSubmit}>
+          <input className="commentInput" placeholder="댓글을 입력하세요..." 
+          onChange={(event) => setCommentIput(event.target.value)}
+          />
+          <button className="commentButton" disabled="" >
+            게 시
+          </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div className="main-right">
+      <div className="userInfoWrap">
+        <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/profile-insta.jpeg" className="profile" />
+        <div className="nicknameAndName">
+          <h3>hyun._.gus</h3>
+          <p>위코드 38기 최현</p>
+        </div>
+      </div>
+      <div className="storyWrap">
+        <div className="storyNav">
+          <span>스토리</span>
+          <p>모두 보기</p>
+        </div>
+        <div className="userWrap">
+          <div className="userN">
+            <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/user1.jpeg" className="rightProfile" alt="user1" />
+            <div className="userIdTime">
+              <h4>hyun._.gus</h4>
+              <p>5분 전</p>
+            </div>
+          </div>
+          <div className="userN">
+            <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/user2.jpeg" className="rightProfile" alt="user1" />
+            <div className="userIdTime">
+              <h4>yougettheswag</h4>
+              <p>16분 전</p>
+            </div>
+          </div>
+          <div className="userN">
+            <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/user3.jpeg" className="rightProfile" alt="user1" />
+            <div className="userIdTime">
+              <h4>Front_end_devloper</h4>
+              <p>2시간 전</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="suggestWrap">
+        <div className="suggestNav">
+          <span>회원님을 위한 추천</span>
+          <p>모두 보기</p>
+        </div>
+        <div className="suggestUserWrap">
+          <div className="suggestUserN">
+            <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/user3.jpeg" className="rightProfile" alt="user1" />
+            <div className="userIdTime">
+              <h4>we123</h4>
+              <p>abcdefg님 외..</p>
+            </div>
+            <span className="follow">팔로우</span>
+          </div>
+          <div className="suggestUserN">
+            <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/user3.jpeg" className="rightProfile" alt="user1" />
+            <div className="userIdTime">
+              <h4>co123</h4>
+              <p>yougettheswag 님 외..</p>
+            </div>
+            <span className="follow">팔로우</span>
+          </div>
+          <div className="suggestUserN">
+            <img src="/Users/hyun/Desktop/38-React-Westagram-5/src/assets/images/choihyun/user3.jpeg" className="rightProfile" alt="user1" />
+            <div className="userIdTime">
+              <h4>de123</h4>
+              <p>sukgodang 님 외..</p>
+            </div>
+            <span className="follow">팔로우</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</>
+
   );
 }
-
 export default MainHyun;
