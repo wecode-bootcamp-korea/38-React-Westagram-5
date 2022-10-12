9699958fbc3f2b0152c70f88623be7f3da9d79f4
yoonsue <mrokr0328@gmail.com>
2022-10-12 13:40:12 +0900

    Add: 댓글 삭제 기능 구현/ Feeds 컴포넌트 내에서 실행되도록 수정

--- src/pages/yoonsue/Main/Main.js
+++ src/pages/yoonsue/Main/Main.js
@@ -1,22 +1,50 @@
 import React, { useState } from 'react';
+import Nav from '../../../components/Nav/Nav';
 import './Main.scss';
+
+function IconsUnderFeedIMG() {
+  const heartOnClick = e => {
+    e.target.style.fontVariationSettings = "'FILL'=1";
+  };
+  return (
+    <div className="icon_under_img">
+      <div className="main_icon">
+        <span className="material-symbols-rounded icon" onClick={heartOnClick}>
+          favorite
+        </span>
+        <span className="material-symbols-rounded icon">mode_comment</span>
+        <span className="material-symbols-rounded icon">ios_share</span>
+      </div>
+      <span className="material-symbols-rounded icon">bookmark</span>
+    </div>
+  );
+}
 
 function Feeds() {
   const [comment, setComment] = useState(''); //input창에 입력하는 즉시 업데이트
-  const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState([]); //qo
+
   const onChange = event => {
     const newComment = event.target.value;
     setComment(newComment);
   };
+
   const isDisabled = () => {
     return comment.trim().length > 0 ? false : true;
   };
+
   const submitComment = event => {
     if (!event.nativeEvent.isComposing && comment.length > 0) {
       event.preventDefault();
       setComments(comments => [...comments, comment]);
       setComment('');
     } else if (!comment.length) event.preventDefault();
+  };
+
+  const deleteComment = index => {
+    const newComments = comments;
+    newComments.splice(index, 1);
+    setComments([...newComments]);
   };
 
   return (
@@ -39,16 +67,7 @@ function Feeds() {
               />
             </div>
           </div>
-          <div className="icon_under_img">
-            <div className="main_icon">
-              <span className="material-symbols-rounded icon">favorite</span>
-              <span className="material-symbols-rounded icon">
-                mode_comment
-              </span>
-              <span className="material-symbols-rounded icon">ios_share</span>
-            </div>
-            <span className="material-symbols-rounded icon">bookmark</span>
-          </div>
+          <IconsUnderFeedIMG></IconsUnderFeedIMG>
         </div>
         <div className="count-likes">
           <div className="liked_num_people"></div>
@@ -63,11 +82,22 @@ function Feeds() {
             <span>더 보기</span>
           </div>
           {/* commentList */}
-          <CommentList
-            className="comment_new"
-            comments={comments}
-            setCommentArr={setComments}
-          />
+          <ul className="comment_new">
+            {comments.map((comment, index) => (
+              <li key={index} className="comment_new_box">
+                <div>
+                  <span className="userID">userId</span>
+                  <div>{comment}</div>
+                </div>
+                <button
+                  className="delete"
+                  onClick={index => deleteComment(index)}
+                >
+                  x
+                </button>
+              </li>
+            ))}
+          </ul>
         </div>
         <div className="timestamp">42분 전</div>
       </article>
@@ -92,45 +122,10 @@ function Feeds() {
   );
 }
 
-function CommentList(props) {
-  const commentArr = props.comments;
-  return (
-    <ul className={props.className}>
-      {commentArr.map((comment, index) => {
-        return (
-          <Comment comment={comment} comments={props.comments} key={index} />
-        );
-      })}
-    </ul>
-  );
-}
-
-function Comment(props) {
-  console.log(props);
-  // const deleteComment = index => {
-  //   props.comments.filter((_, i) => {
-  //     return i !== index;
-  //   });
-  // };
-  const deleteComment = event => {
-    props.comments.filter(item => {
-      return item !== event.target.value.parent;
-    });
-  };
-  return (
-    <li>
-      <span className="userId">hello</span>
-      <span className="comment_new_box">{props.comment}</span>
-      <button onClick={deleteComment} className="delete">
-        X
-      </button>
-    </li>
-  );
-}
-
 function MainSue() {
   return (
     <div className="body">
+      <Nav />
       <main>
         <div className="main">
           <Feeds />
