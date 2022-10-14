e3fe2a5f6ca2365bde85f1ec60778fb76526bd62
KMK99cone <105492581+KMK99cone@users.noreply.github.com>
2022-10-14 10:22:55 +0900

    로그인&회원가입실습

--- src/pages/minkyung/Login/Login.js
+++ src/pages/minkyung/Login/Login.js
@@ -1,69 +1,147 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../Login/Login.scss';
+import '../Main/Main';
 
-import React, { useState }  from "react";
-import { useNavigate } from "react-router-dom";
-import "./Login.scss";
-import "../Main/Main"
+function Login() {
+  const navigate = useNavigate();
+  const [id, setId] = useState('');
+  const saveUserId = event => {
+    setId(event.target.value);
+  };
+  const [pw, setPw] = useState('');
+  const saveUserPw = event => {
+    setPw(event.target.value);
+  };
+  const isPW = id.includes('@') && pw.length >= 5;
+  const BC = isPW ? 'blue' : '#C0DFFD';
 
-function Login () {
-    const navigate=useNavigate();
-    const [id, setId] = useState('');
-    const saveUserId = (event)=> {
-        setId(event.target.value);
-    };
-    const [pw, setPw] = useState('');
-    const saveUserPw = (event)=> {
-        setPw(event.target.value);
-    };
-    const isPW = id.includes('@') && pw.length>=5 ;
-    const enterPW = e => {
-        if (e.key === 'Enter') {
-            clickPW();
+  const enterPW = e => {
+    if (e.key === 'Enter') {
+      clickLogin();
+    }
+  };
+  const clickLogin = () => {
+    fetch('http://10.58.52.130:3000/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email: id,
+        password: pw,
+      }),
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
         }
-    };
-    const clickPW = () => {
-        return (isPW ?  navigate('/main'): null )
-    };
-    const BC = ( isPW? 'blue' : '#C0DFFD'  )
-    
-    // const inputValue = 
-
-
-    
-    return(
-     <nav>
-            <div className="line">
-                <div className="westagram">westagram</div>
-                <div className="idpasswordbutton">
-                    <div className="id">
-                        <input id="id" type="text" value={id} onChange={saveUserId}  onKeyPress = {enterPW} placeholder="전화번호,사용자 이름 또는 이메일" />
-                    </div>
-                    <div className="password">
-                        <input id="pw" type="password" value={pw} onChange={saveUserPw}  onKeyPress = {enterPW} placeholder="비밀번호" />
-                    </div>
-                    <button className="login"
-                    style ={{backgroundColor: BC  }}
-                     onClick={ clickPW}              
-                    >
-                    로그인</button>
-                <div className="or">또는</div>
-                <div className="facebook"><i className="fa-brands fa-square-facebook"></i>&nbsp;Facebook으로 로그인</div>
-                <div className="cc"></div>
-                <div className="gotopassword">비밀번호를 잊으셨나요?</div>
-            </div>
-        </div><div className="sign">
-                <div className="sign?">계정이 없으신가요?&nbsp;&nbsp;</div>
-                <div className="singin"
-                    onClick={ () => {
-                        navigate("/Singup");
-                    }}
-                     > 가입하기</div>
-            </div>
-               
-    </nav> 
-
-           
-    ) ;
+        throw new Error('통신실패');
+      })
+      .catch(error => console.log(error))
+      .then(data => {
+        // if (data.message === "SUCCESS") {
+        localStorage.setItem('token', data.accessToken);
+        //   alert("로그인 성공");
+        //   console.log("AA");
+        // } else if (data.message === " INVALIDU_USER_ID") {
+        //   alert("아이디 혹은 비밀번호를 확인해 주세요");
+        // }
+      });
+    const token = localStorage.getItem('token');
+  };
+  const clickSingUp = () => {
+    fetch('http://10.58.52.130:3000/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      body: JSON.stringify({
+        email: id,
+        password: pw,
+      }),
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        throw new Error('통신실패');
+      })
+      .catch(error => console.log(error))
+      .then(data => {
+        if (data.message === 'SUCCESS') {
+          localStorage.setItem('token', data.token);
+          alert('로그인 성공');
+          // console.log("AA");
+        } else if (data.message === ' INVALIDU_USER_ID') {
+          alert('아이디 혹은 비밀번호를 확인해 주세요');
+        }
+      });
+    const token = localStorage.getItem('token');
+  };
+  return (
+    <nav>
+      <div className="line">
+        <div className="westagram">westagram</div>
+        <div className="idpasswordbutton">
+          <div className="id">
+            <input
+              id="id"
+              type="text"
+              value={id}
+              style={{ backgroundColor: '#fafafa' }}
+              onChange={saveUserId}
+              onKeyPress={enterPW}
+              placeholder="전화번호,사용자 이름 또는 이메일"
+            />
+          </div>
+          <div className="password">
+            <input
+              id="pw"
+              type="password"
+              value={pw}
+              style={{ backgroundColor: '#fafafa' }}
+              onChange={saveUserPw}
+              onKeyPress={enterPW}
+              placeholder="비밀번호"
+            />
+          </div>
+          <button
+            className="button"
+            style={{ backgroundColor: BC }}
+            onClick={clickLogin}
+          >
+            로그인
+          </button>
+          <button
+            className="button"
+            style={{ backgroundColor: BC }}
+            onClick={clickSingUp}
+          >
+            회원가입
+          </button>
+          <div className="or">또는</div>
+          <div className="facebook">
+            <i className="fa-brands fa-square-facebook"></i>&nbsp;Facebook으로
+            로그인
+          </div>
+          <div className="cc"></div>
+          <div className="gotopassword">비밀번호를 잊으셨나요?</div>
+        </div>
+      </div>
+      <div className="sign">
+        <div className="sign?">계정이 없으신가요?&nbsp;&nbsp;</div>
+        <div
+          className="singup"
+          onClick={() => {
+            navigate('/Singup');
+          }}
+        >
+          가입하기
+        </div>
+      </div>
+    </nav>
+  );
 }
-
 
 export default Login;
--- src/pages/minkyung/Main/Main.js
+++ src/pages/minkyung/Main/Main.js
@@ -1,141 +1,231 @@
-import React from "react";
-import "./Main.scss"
-import { useState } from "react";
-import "../Login/Login"
+import React, { useEffect } from 'react';
+import '../Main/Main.scss';
+import { useState } from 'react';
+// import "../Login/Login";
+import { useNavigate } from 'react-router-dom';
 
-function Main () {
-	const [reply,setReply] = useState('');
-	const replyreplace = reply.replace(/ /g,'');
-	const isreplyempty = (replyreplace<=0 ? true :false);
-	const resultColor = (  isreplyempty ? '#9C9C9C': 'blue'  )
-	const saveReply = (event) => {setReply(event.target.value);};
+function Main() {
+  const [userInfoList, setUserInfoList] = useState([]);
+  const [reply, setReply] = useState('');
+  const [replyArray, setReplyArray] = useState([]);
+  const navigate = useNavigate('');
 
+  useEffect(() => {
+    fetch('/data/userInfoList.json')
+      .then(response => response.json())
+      .then(result => setUserInfoList(result));
+  }, []);
 
+  const replyreplace = reply.replace(/ /g, '');
+  const isreplyempty = replyreplace <= 0 ? true : false;
+  const resultColor = isreplyempty ? '#9C9C9C' : 'blue';
 
+  //댓글구현
 
-// console.log(saveReply)
+  const saveReply = e => {
+    setReply(e.target.value);
+  };
 
-	//댓글구현
-	const [replyArray, setReplyArray] = useState([]);
+  const replySet = event => {
+    event.preventDefault();
+    if (reply === '') {
+      return;
+    } else {
+      setReplyArray(replyList => [...replyList, reply]);
+      setReply('');
+    }
+  };
 
+  return (
+    <nav>
+      <div className="topbar">
+        <div className="westaicontext">
+          <div className="westaicon">
+            <i className="fa-brands fa-instagram">&nbsp;&nbsp;</i>
+          </div>
+          <div className="westatext">westagram </div>
+        </div>
 
-	const registReply = event =>{
-		event.preventDefault();
-		if(reply === ''){
-			return;
-		}
-		setReplyArray(replyValueList => [...replyValueList,reply]);
-		setReply('');
-	}
+        <div className="finder">
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input type="text" placeholder="검색" />
+        </div>
 
-	const deleteReply = event =>{
-		replyArray.pop(1)
-	}
-
-
-	return (
-	<nav>
-        <div className="topbar">
-			<div className="westaicontext">
-      			<div className="westaicon">      
-        			<i className="fa-brands fa-instagram">&nbsp;&nbsp;</i></div>
-      			<div className="westatext">westagram  </div>
-		    </div>
-		
-            <div className="finder">
-             <i className="fa-solid fa-magnifying-glass"></i><input type="text" placeholder= "검색" /> 
-			</div>
-
-   		   <div className="icon">
-  		    <div className="c-1"><img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"  alt="profile"/></div>
-     		<div className="c-2"><img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"  alt="profile"/></div>
-      		<div className="c-3"><img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"  alt="profile"/></div>
-    	   </div>  
-		 </div>
-		
-		 <div className = "mains">                                                                
- 			 <div className="content">
-   				 <div className ="main_content">
-     				 <div className="main">
-      				  <img className="profile-o" src="images/pexels-derek-randolph-12930643.jpg"  alt="profile"/>
-       					<div className="main_text">&nbsp;&nbsp;jjj_mj</div>  
-        				<div className="main_icon"><i className="fa-solid fa-ellipsis"></i></div>
-					 </div>
-      				<div className="main-img"><img className="main_img" src="images/pexels-arthouse-studio-4344533.jpg"  alt="profile"/></div>
-      				<div className="in-icon">
-       					<div className="inicon-1">
-         					<div className="ic-1"><i className="fa-regular fa-heart"></i></div>
-          					<div className="ic-2"><i className="fa-regular fa-comment"></i></div>
-              				<div className="ic-3"><i className="fa-solid fa-arrow-up-from-bracket"></i></div>
-        				</div>
-        				<div className="inicon-2">
-          					<div className="ic-4"><i className="fa-regular fa-bookmark"></i></div>
-       					</div>
-      				</div>
-     				 <div className ="extra">
-      				  <div className ="likes">
-       					 aineworld님 외 10명이 좋아합니다
-       				  </div>
-      
-       				  <div className ="text">
-         				 <div className = "mid">
-        				   	<div className ="mid_1"> wecode&nbsp;</div> <div className = "mid_2"> dream room</div>
-        				   	<div className = "mlikes"></div>
-        				  	</div>
-							<div className = "w_reply">
-								{replyArray.map((sth=> (
-									<div className = "w_replyArray">
-									<div className ="w_reply_id"> ys_dd002 </div> 
-									<div className = "w_reply_txt" key={sth}>{sth} </div>
-									<div className = "w_icon"> <i className="fa-regular fa-heart" />&nbsp; </div>
-									<div className= "w_delete" onClick={deleteReply}> &nbsp;삭제&nbsp; </div>
-									</div>
-								)))}
-						    </div>
-      				  </div>
-        			 <form className = "reply" onSubmit={registReply}>
-         				 <input id="reply" 
-						 type="text" 
-						 value={reply}
-						 onChange={saveReply}  
-						 placeholder="댓글달기.." 
-						 
-						 />
-          				 <button className ="replybutton"
-						 style={{ color : resultColor}}
-						 onClick={registReply}
-						 >게시</button>
-     				 </form>
-     			 </div>
-  			  </div>
-   			 </div>
-  
-  <div className ="sidebar">
-    <div className="you">
-      <img className="profile-i" src ="images/girl-gbeeb67702_1280.jpg" alt="profile" /> 
-      <div className="yall">
-      <div className="yname">ys_dd002</div>
-      <div className="ycontent">flower</div>
+        <div className="icon">
+          <div className="c-1">
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+              alt="profile"
+            />
+          </div>
+          <div className="c-2">
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+              alt="profile"
+            />
+          </div>
+          <div
+            className="c-3"
+            onClick={() => {
+              navigate('/Singup');
+            }}
+          >
+            <img
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+              alt="profile"
+            />
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="story">
-      <div className= "s_main">
-        <div className="story_story">스토리</div>
-        <div className="story_all">모두 보기</div>
+
+      <div className="mains">
+        <div className="content">
+          {userInfoList.map(userInfoList => (
+            <div className="main_content" key={userInfoList.id}>
+              <div className="main">
+                <img
+                  className="profile-o"
+                  src="images/minkyung/pexels-derek-randolph-12930643.jpg"
+                  alt="profile"
+                />
+                <div className="main_text" key={AsideData.id}>
+                  &nbsp;&nbsp;{userInfoList.username}
+                </div>
+                <div className="main_icon">
+                  <i className="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+              <div className="main-img">
+                <img
+                  className="main_img"
+                  src="images/minkyung/pexels-arthouse-studio-4344533.jpg"
+                  alt="profile"
+                />
+              </div>
+              <div className="in-icon">
+                <div className="inicon-1">
+                  <div className="ic-1">
+                    <i className="fa-regular fa-heart"></i>
+                  </div>
+                  <div className="ic-2">
+                    <i className="fa-regular fa-comment"></i>
+                  </div>
+                  <div className="ic-3">
+                    <i className="fa-solid fa-arrow-up-from-bracket"></i>
+                  </div>
+                </div>
+                <div className="inicon-2">
+                  <div className="ic-4">
+                    <i className="fa-regular fa-bookmark"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="extra">
+                <div className="likes">aineworld님 외 10명이 좋아합니다</div>
+
+                <div className="text">
+                  <div className="mid">
+                    <div className="mid_1"> wecode&nbsp;</div>{' '}
+                    <div className="mid_2"> dream room</div>
+                    <div className="mlikes"></div>
+                  </div>
+                  <div className="w_reply">
+                    {replyArray.map(sth => (
+                      <form onSubmit={replySet} key={sth}>
+                        <div className="w_replyArray">
+                          <div className="w_reply_id"> ys_dd002 </div>
+                          <div className="w_reply_txt">{sth} </div>
+                          <div className="w_icon">
+                            {' '}
+                            <i className="fa-regular fa-heart" />
+                            &nbsp;{' '}
+                          </div>
+                          <div className="w_delete"> &nbsp;삭제&nbsp; </div>
+                        </div>
+                      </form>
+                    ))}
+                  </div>
+                </div>
+                <form className="reply">
+                  <input
+                    id="reply"
+                    type="text"
+                    placeholder="댓글달기.."
+                    onChange={saveReply}
+                    value={reply}
+                  />
+                  <button
+                    className="replybutton"
+                    style={{ color: resultColor }}
+                    onClick={replySet}
+                  >
+                    게시
+                  </button>
+                </form>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="sidebar">
+          <div className="you">
+            <img
+              className="profile-i"
+              src="images/minkyung/girl-gbeeb67702_1280.jpg"
+              alt="profile"
+            />
+            <div className="yall">
+              <div className="yname">ys_dd002</div>
+              <div className="ycontent">flower</div>
+            </div>
+          </div>
+          <div className="story">
+            <div className="s_main">
+              <div className="story_story">스토리</div>
+              <div className="story_all">모두 보기</div>
+            </div>
+            <div className="story_icons">
+              {userInfoList.map(userInfoList => (
+                <div className="story_icon">
+                  <img
+                    className="profile-other"
+                    src={userInfoList.src}
+                    alt="profile"
+                  />
+                  <div className="profile-5" key={userInfoList.id}>
+                    {' '}
+                    {userInfoList.username}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="AD">
+            {AsideData.map(AsideData => (
+              <div className="asideData" key={AsideData.id}>
+                &nbsp;{AsideData.dataName}&nbsp;
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="story_icons">
-       <div className="story_icon"> <img className="profile-1" src="images/pexels-djordje-cvetkovic-6389849.jpg"  alt="profile"/><div className="profile-5"> dd_ss003</div></div>
-       <div className="story_icon"> <img className="profile-2" src="images/cat-g69fa01b87_1920.jpg"  alt="profile"/><div className="profile-5">hihi_0222</div></div>
-       <div className="story_icon"><img className="profile-3" src="images/pexels-si-luan-pham-8778462.jpg"  alt="profile"/><div className="profile-5">dkik</div></div>
-       <div className="story_icon"><img className="profile-4" src="images/fashion-g776ec8afd_1920.jpg"  alt="profile"/><div className="profile-5">leeo</div></div>
-      </div>
-    </div>
-    <div className="foryou"></div>
-  </div>
-</div>
-</nav>
-	
-		) ;
+    </nav>
+  );
 }
+
+const AsideData = [
+  { id: 1, dataName: '소개' },
+  { id: 2, dataName: '도움말' },
+  { id: 3, dataName: '홍보센터' },
+  { id: 4, dataName: 'API' },
+  { id: 5, dataName: '채용 정보' },
+  { id: 6, dataName: '개인정보처리방침' },
+  { id: 7, dataName: '약관' },
+  { id: 8, dataName: '위치' },
+  { id: 9, dataName: '인기계정' },
+  { id: 10, dataName: '해시태그' },
+  { id: 11, dataName: '언어' },
+];
 
 export default Main;
